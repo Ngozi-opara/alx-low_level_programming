ab907c1f95acffc5813555a73f41d68a7ed9d5c6
Hannah--- <87046881+Ngozi-opara@users.noreply.github.com>
2022-09-10 18:36:10 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
-int main(void)  
+int main(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
 printf("%i", i);
-} 
+}
 printf("\n");
-return (0);  
+return (0);
 }
